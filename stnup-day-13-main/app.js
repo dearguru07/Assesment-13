b496deee77222b3ca247da993d4f892063122628
dearguru07 <pangalaguruyadav101@gmail.com>
2025-02-04 10:24:48 +0530

    done

--- stnup-day-13-main/app.js
+++ stnup-day-13-main/app.js
@@ -1,28 +1,36 @@
-console.log('clikced the download btn')
-console.log('server is fecth')
-console.log('movie is downloading')
-console.log('Movie is downloaded , stored to ur divice')
+// console.log('clikced the download btn')
+// console.log('server is fecth')
+// console.log('movie is downloading')
+// console.log('Movie is downloaded , stored to ur divice')
 
-
-async function dataFetch() {
-    const data =await fetch("https://dummyjson.com/products")
-    .then((data) =>{
-        const originalData=data.json()
-        return originalData
-    }).then((originalData)=>{
-        document.getElementById('headiing').innerHTML=originalData[1].text
-    })
-    console.log(originalData)
-}
-dataFetch()
-
-
+// async function dataFetch() {
+//     const data =await fetch("https://dummyjson.com/products")
+//     .then((data) =>{
+//         const originalData=data.json()
+//         return originalData
+//     }).then((originalData)=>{
+//         document.getElementById('headiing').innerHTML=originalData[1].text
+//     })
+//     console.log(originalData)
+// }
+// dataFetch()
 
 function hello(){
     let data=fetch('https://dummyjson.com/products')
-    console.log(data)
+    // console.log(data)
     .then((data)=>{
         let originalData=data.json()
-        console.log(originalData)
+        return originalData;
+    }).then((originalData)=>{
+        console.log(originalData.products[1].stock)
     })
 }
+
+// function hello() {
+//   let data = fetch("https://dummyjson.com/products")
+//     // console.log(data)
+//     .then((data) => {
+//       let originalData = data.json();
+//       console.log(originalData);
+//     });
+// }
